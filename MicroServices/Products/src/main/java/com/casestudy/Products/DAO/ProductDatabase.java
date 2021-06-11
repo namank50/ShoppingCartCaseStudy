5b95c7da2ac9e96c611fa9b8cbba2251bf8c9f67
naman <naman@DESKTOP-1SBKM3G>
2021-06-11 09:57:16 +0530

    Completed Product Microservice

--- MicroServices/Products/src/main/java/com/casestudy/Products/DAO/ProductDatabase.java
+++ MicroServices/Products/src/main/java/com/casestudy/Products/DAO/ProductDatabase.java
@@ -1,9 +1,17 @@
 package com.casestudy.Products.DAO;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.casestudy.Products.Models.Products;
 
+@Repository
 public interface ProductDatabase extends MongoRepository<Products, String>{
+
+	List<Products> findByDealerId(String DealerId);
+	List<Products> findByCategory(String category);
+	Products findByPName(String PName);
 
 }
